2fa5acf67be3d541cf87327da910d0511b41c3e4
Igor Melão <igormelao@gmail.com>
2021-03-13 19:44:02 +0000

    Configura Spring Fox Swagger to generate automatically all documentation fo API; Create new class to configure CORS inside of package security

--- src/main/java/com/doctorlabel/DoctorlabelLabellingServiceApplication.java
+++ src/main/java/com/doctorlabel/DoctorlabelLabellingServiceApplication.java
@@ -1,35 +1,18 @@
 package com.doctorlabel;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSwagger2
 public class DoctorlabelLabellingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DoctorlabelLabellingServiceApplication.class, args);
-	}
-	
-	@Value("${allowed.origin}")
-	private String allowedOrigin;
-	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-				.allowedOrigins(allowedOrigin, "http://localhost:8080", "http://localhost:8081")
-				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-				.allowedHeaders("*");
-			}
-		};
 	}
 
 }
